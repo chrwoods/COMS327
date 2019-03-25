37d4e8d7b44bfe8fde26b2c8b728223567463c85
Christopher Woods <chrisscottwoods@gmail.com>
2019-03-25 17:40:33 -0500

    Updated drats.h to C++, changed status text to be constant

--- drats.h
+++ drats.h
@@ -1,8 +1,5 @@
 //In case you're curious why this is called drats.h, it's because in cartoons they often say "Drats!" in place of "Curses!", often followed by some form of "Foiled again!". If you think that's a genius naming scheme for this file... you and me both, man.
 
-//#include <curses.h>
-
-//void drats(struct dungeon *rlg){
 void drats(){
   initscr(); //if you don't do this, the terrorists win
   cbreak(); //changes input to character-to-character basis
@@ -14,58 +11,58 @@ void drats(){
   //rlg->status = newwin(3, WIDTH, HEIGHT, 0);
 }
 
-void update_background(struct dungeon *rlg){
+void Dungeon::update_background(){
   //place rocks and empty spaces
   for(int i = 0; i < HEIGHT; i++){
     for(int j = 0; j < WIDTH; j++){
-      if((*rlg).map[i][j] == 0) rlg->background[i][j] = '#';
-      else rlg->background[i][j] = ' ';
+      if(map[i][j] == 0) background[i][j] = '#';
+      else background[i][j] = ' ';
     }
   }
   //place rooms
-  for(int n = 0; n < (*rlg).num_rooms; n++){
-    for(int i = 0; i < (*rlg).rooms[n].height; i++){
-      for(int j = 0; j < (*rlg).rooms[n].width; j++){
-	rlg->background[(*rlg).rooms[n].row + i][(*rlg).rooms[n].col + j] = '.';
+  for(int n = 0; n < num_rooms; n++){
+    for(int i = 0; i < rooms[n].height; i++){
+      for(int j = 0; j < rooms[n].width; j++){
+	background[rooms[n].row + i][rooms[n].col + j] = '.';
       }
     }
   }
   //place up stairs
-  for(int i = 0; i < (*rlg).num_up; i++){
-    rlg->background[(*rlg).up[i].row][(*rlg).up[i].col] = '<';
+  for(int i = 0; i < num_up; i++){
+    background[up[i].row][up[i].col] = '<';
   }
   //place down stairs
-  for(int i = 0; i < (*rlg).num_down; i++){
-    rlg->background[(*rlg).down[i].row][(*rlg).down[i].col] = '>';
+  for(int i = 0; i < num_down; i++){
+    background[down[i].row][down[i].col] = '>';
   }
 }
 
-void print_map(struct dungeon *rlg){
+void Dungeon::print_map(){
   //wclear(rlg->world);
   //place background
   for(int i = 0; i < HEIGHT; i++){
     for(int j = 0; j < WIDTH; j++){
-      mvaddch(i, j, rlg->background[i][j]);
+      mvaddch(i, j, background[i][j]);
     }
   }
   //place player character
-  mvaddch((*rlg).pc.row, (*rlg).pc.col, '@');
+  mvaddch(pc.row, pc.col, '@');
   //place monsters
   char monstersyms[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-  for(int i = 0; i < rlg->num_monsters; i++){
-    if((rlg->monsters)[i].speed == 0) continue; //dead monster
-    mvaddch((rlg->monsters)[i].row, (rlg->monsters)[i].col, monstersyms[(rlg->monsters)[i].type]);
+  for(int i = 0; i < num_monsters; i++){
+    if(monsters[i].speed == 0) continue; //dead monster
+    mvaddch(monsters[i].row, monsters[i].col, monstersyms[monsters[i].type]);
   }
   refresh();
 }
 
-void stard(){
+void stard(){ //drats backwards
   //delwin(rlg->world);
   //delwin(rlg->status);
   endwin();
 }
 
-void update_status_text(char* text){
+void update_status_text(char const * text){
   for(int i = 0; i < WIDTH; i++) mvaddch(HEIGHT, i, ' '); 
   mvprintw(HEIGHT, 0, text);
   refresh();
