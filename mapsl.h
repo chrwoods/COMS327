efd07d3974d4d7fa54f97acbdefa5ee90f580079
Christopher Woods <chrisscottwoods@gmail.com>
2019-03-25 14:52:53 -0500

    Converted mapsl.h to C++

--- mapsl.h
+++ mapsl.h
@@ -1,48 +1,46 @@
-//#include "structs.h"
-
-void save_map(char* filepath, struct dungeon *rlg){
+void Dungeon::save_map(char* filepath){
   FILE *fp;
   fp = fopen(filepath, "w");
   fwrite("RLG327-S2019", sizeof(char), 12, fp);
   uint32_t num32 = htobe32(0);
   fwrite(&num32, sizeof(num32), 1, fp);
-  num32 = htobe32(1708 + (*rlg).num_rooms * 4 + (*rlg).num_up * 2 + (*rlg).num_down * 2);
+  num32 = htobe32(1708 + num_rooms * 4 + num_up * 2 + num_down * 2);
   fwrite(&num32, sizeof(num32), 1, fp);
-  fwrite(&((*rlg).pc.col), 1, 1, fp);
-  fwrite(&((*rlg).pc.row), 1, 1, fp);
+  fwrite(&(pc.col), 1, 1, fp);
+  fwrite(&(pc.row), 1, 1, fp);
   //write the map
   //fwrite(map8, 1, HEIGHT * WIDTH, fp); //this works too
   for(int i = 0; i < HEIGHT; i++){
     for(int j = 0; j < WIDTH; j++){
-      fwrite(&((*rlg).map[i][j]), 1, 1, fp);
+      fwrite(&(map[i][j]), 1, 1, fp);
     }
   }
   //write the rooms
-  uint16_t num16 = htobe16((*rlg).num_rooms);
+  uint16_t num16 = htobe16(num_rooms);
   fwrite(&num16, sizeof(num16), 1, fp);
-  for(int i = 0; i < (*rlg).num_rooms; i++){
-    fwrite(&((*rlg).rooms[i].col), 1, 1, fp);
-    fwrite(&((*rlg).rooms[i].row), 1, 1, fp);
-    fwrite(&((*rlg).rooms[i].width), 1, 1, fp);
-    fwrite(&((*rlg).rooms[i].height), 1, 1, fp);
+  for(int i = 0; i < num_rooms; i++){
+    fwrite(&(rooms[i].col), 1, 1, fp);
+    fwrite(&(rooms[i].row), 1, 1, fp);
+    fwrite(&(rooms[i].width), 1, 1, fp);
+    fwrite(&(rooms[i].height), 1, 1, fp);
   }
   //write stairs
-  num16 = htobe16((*rlg).num_up);
+  num16 = htobe16(num_up);
   fwrite(&num16, sizeof(num16), 1, fp);
-  for(int i = 0; i < (*rlg).num_up; i++){
-    fwrite(&((*rlg).up[i].col), 1, 1, fp);
-    fwrite(&((*rlg).up[i].row), 1, 1, fp);
+  for(int i = 0; i < num_up; i++){
+    fwrite(&(up[i].col), 1, 1, fp);
+    fwrite(&(up[i].row), 1, 1, fp);
   }
-  num16 = htobe16((*rlg).num_down);
+  num16 = htobe16(num_down);
   fwrite(&num16, sizeof(num16), 1, fp);
-  for(int i = 0; i < (*rlg).num_down; i++){
-    fwrite(&((*rlg).down[i].col), 1, 1, fp);
-    fwrite(&((*rlg).down[i].row), 1, 1, fp);
+  for(int i = 0; i < num_down; i++){
+    fwrite(&(down[i].col), 1, 1, fp);
+    fwrite(&(down[i].row), 1, 1, fp);
   }
   fclose(fp);
 }
 
-int load_map(char* filepath, struct dungeon *rlg){
+int Dungeon::load_map(char* filepath){
   FILE *fp;
   fp = fopen(filepath, "r");
   if(fp == 0){
@@ -66,30 +64,30 @@ int load_map(char* filepath, struct dungeon *rlg){
   fread(&num32, sizeof(uint32_t), 1, fp);
   num32 = be32toh(num32); //filesize for later
   //read player character
-  fread(&((*rlg).pc.col), 1, 1, fp);
-  fread(&((*rlg).pc.row), 1, 1, fp);
+  fread(&(pc.col), 1, 1, fp);
+  fread(&(pc.row), 1, 1, fp);
   //read map grid
   for(int i = 0; i < HEIGHT; i++){
     for(int j = 0; j < WIDTH; j++){
       uint8_t num8;
       fread(&num8, 1, 1, fp);
-      (*rlg).map[i][j] = num8;
+      map[i][j] = num8;
     }
   }
   //read number of rooms
   uint16_t num16;
   fread(&num16, sizeof(uint16_t), 1, fp);
-  (*rlg).num_rooms = be16toh(num16);
-  (*rlg).rooms = malloc((*rlg).num_rooms * sizeof(struct room));
+  num_rooms = be16toh(num16);
+  rooms = (Room*)realloc(rooms, num_rooms * sizeof(Room));
   //load rooms
-  for(int n = 0; n < (*rlg).num_rooms; n++){
-    fread(&((*rlg).rooms[n].col), 1, 1, fp);
-    fread(&((*rlg).rooms[n].row), 1, 1, fp);
-    fread(&((*rlg).rooms[n].width), 1, 1, fp);
-    fread(&((*rlg).rooms[n].height), 1, 1, fp);
-    for(int i = (*rlg).rooms[n].row; i < (*rlg).rooms[n].height + (*rlg).rooms[n].row; i++){
-      for(int j = (*rlg).rooms[n].col; j < (*rlg).rooms[n].width + (*rlg).rooms[n].col; j++){
-	if((*rlg).map[i][j] > 0){
+  for(int n = 0; n < num_rooms; n++){
+    fread(&(rooms[n].col), 1, 1, fp);
+    fread(&(rooms[n].row), 1, 1, fp);
+    fread(&(rooms[n].width), 1, 1, fp);
+    fread(&(rooms[n].height), 1, 1, fp);
+    for(int i = rooms[n].row; i < rooms[n].height + rooms[n].row; i++){
+      for(int j = rooms[n].col; j < rooms[n].width + rooms[n].col; j++){
+	if(map[i][j] > 0){
 	  printf("Room placement data does not match given map, unable to load map.\n");
 	  return -3;
 	}
@@ -99,20 +97,20 @@ int load_map(char* filepath, struct dungeon *rlg){
   }
   //read staircases
   fread(&num16, sizeof(num16), 1, fp);
-  (*rlg).num_up = be16toh(num16);
-  (*rlg).up = malloc((*rlg).num_up * sizeof(struct obj));
-  for(int i = 0; i < (*rlg).num_up; i++){
+  num_up = be16toh(num16);
+  up = (Obj*)realloc(up, num_up * sizeof(Obj));
+  for(int i = 0; i < num_up; i++){
     uint8_t num8;
-    fread(&((*rlg).up[i].col), 1, 1, fp);
-    fread(&((*rlg).up[i].row), 1, 1, fp);
+    fread(&(up[i].col), 1, 1, fp);
+    fread(&(up[i].row), 1, 1, fp);
     num32 -= 2;
   }
   fread(&num16, sizeof(num16), 1, fp);
-  (*rlg).num_down = be16toh(num16);
-  (*rlg).down = malloc((*rlg).num_down * sizeof(struct obj));
-  for(int i = 0; i < (*rlg).num_down; i++){
-    fread(&((*rlg).down[i].col), 1, 1, fp);
-    fread(&((*rlg).down[i].row), 1, 1, fp);
+  num_down = be16toh(num16);
+  down = (Obj*)realloc(down, num_down * sizeof(Obj));
+  for(int i = 0; i < num_down; i++){
+    fread(&(down[i].col), 1, 1, fp);
+    fread(&(down[i].row), 1, 1, fp);
     num32 -= 2;
   }
   //check if filesize is correct and close file
