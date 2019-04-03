9292862c22a6bcb6bf0d92e4db5cb123a003ed39
Christopher Woods <chrisscottwoods@gmail.com>
2019-04-03 17:17:13 -0500

    Added printing of loaded items

--- loader.h
+++ loader.h
@@ -234,3 +234,181 @@ void Dungeon::print_monster(int num){
   cout << "Rarity: " << std::to_string(monsters[num].rarity) << endl;
   cout << endl;
 }
+
+int Dungeon::load_items(string filepath){
+  ifstream fp(filepath);
+  if(!fp.is_open()){
+    cout << "File to load was not found at filepath: " << filepath << endl;
+    return -1;
+  }
+  string line;
+  getline(fp, line);
+  if(line.compare("RLG327 OBJECT DESCRIPTION 1") != 0){
+    cout << "Filetype marker unrecognized, unable to load items." << endl;
+    return -2;
+  }
+  int loadreturn;
+  while(fp.is_open()){
+    while(line.compare("BEGIN OBJECT") != 0 && !fp.eof()) getline(fp, line);
+    if(fp.eof()) break;
+    loadreturn = load_item(&fp); //nothing is done with loadreturn right now, since if an item fails to load it simply isn't added to the vector
+  }
+  fp.close();
+  return 0;
+}
+
+int Dungeon::load_item(ifstream *fp){
+  string line;
+  uint16_t complete = 0;
+  Item item;
+  while(1){
+    getline(*fp, line);
+    if(fp->eof()) return -2; //reached end of file!?
+    if(line.compare("END") == 0) break;
+    string header = line.substr(0, line.find_first_of(" "));
+    if(header.compare("NAME") == 0){
+      if((complete & 0x1) == 0x1) return -1; //field already filled
+      item.name = line.substr(line.find_first_of(" ") + 1);
+      complete = complete | 0x1;
+    } else if(header.compare("DESC") == 0){
+      if((complete & 0x2) == 0x2) return -1; //field already filled
+      item.desc = "";
+      getline(*fp, line);
+      while(line.compare(".") != 0){
+	item.desc.append(line + '\n');
+	getline(*fp, line);
+      }
+      complete = complete | 0x2;
+    } else if(header.compare("TYPE") == 0){
+      if((complete & 0x4) == 0x4) return -1; //field already filled
+      item.type = line.substr(line.find_first_of(' ') + 1);
+      complete = complete | 0x4;
+    } else if(header.compare("COLOR") == 0){
+      if((complete & 0x8) == 0x8) return -1; //field already filled
+      string color = line.substr(line.find_first_of(' ') + 1);
+      if(color.compare("BLUE") == 0) item.color = BLUE_PAIR;
+      else if(color.compare("GREEN") == 0) item.color = GREEN_PAIR;
+      else if(color.compare("CYAN") == 0) item.color = CYAN_PAIR;
+      else if(color.compare("RED") == 0) item.color = RED_PAIR;
+      else if(color.compare("MAGENTA") == 0) item.color = MAGENTA_PAIR;
+      else if(color.compare("YELLOW") == 0) item.color = YELLOW_PAIR;
+      else if(color.compare("WHITE") == 0) item.color = WHITE_PAIR;
+      else if(color.compare("BLACK") == 0) item.color = GRAY_PAIR; //use gray for now instead of black
+      else if(color.compare("GRAY") == 0 || color.compare("GREY") == 0) item.color = GRAY_PAIR;
+      else return -3; //found color value not specified
+      complete = complete | 0x8;
+    } else if(header.compare("HIT") == 0){
+      if((complete & 0x10) == 0x10) return -1; //field already filled
+      item.hit.parse(line.substr(line.find_first_of(' ') + 1));
+      complete = complete | 0x10;
+    } else if(header.compare("DAM") == 0){
+      if((complete & 0x20) == 0x20) return -1; //field already filled
+      item.damage.parse(line.substr(line.find_first_of(' ') + 1));
+      complete = complete | 0x20;
+    } else if(header.compare("DODGE") == 0){
+      if((complete & 0x40) == 0x40) return -1; //field already filled
+      item.dodge.parse(line.substr(line.find_first_of(' ') + 1));
+      complete = complete | 0x40;
+    } else if(header.compare("DEF") == 0){
+      if((complete & 0x80) == 0x80) return -1; //field already filled
+      item.def.parse(line.substr(line.find_first_of(' ') + 1));
+      complete = complete | 0x80;
+    } else if(header.compare("WEIGHT") == 0){
+      if((complete & 0x100) == 0x100) return -1; //field already filled
+      item.weight.parse(line.substr(line.find_first_of(' ') + 1));
+      complete = complete | 0x100;
+    } else if(header.compare("SPEED") == 0){
+      if((complete & 0x200) == 0x200) return -1; //field already filled
+      item.speed.parse(line.substr(line.find_first_of(' ') + 1));
+      complete = complete | 0x200;
+    } else if(header.compare("ATTR") == 0){
+      if((complete & 0x400) == 0x400) return -1; //field already filled
+      item.rarity = stoi(line.substr(line.find_first_of(' ') + 1));
+      complete = complete | 0x400;
+    } else if(header.compare("VAL") == 0){
+      if((complete & 0x800) == 0x800) return -1; //field already filled
+      item.value.parse(line.substr(line.find_first_of(' ') + 1));
+      complete = complete | 0x800;
+    } else if(header.compare("ART") == 0){
+      if((complete & 0x1000) == 0x1000) return -1; //field already filled
+      string art = line.substr(line.find_first_of(' ') + 1);
+      if(art.compare("TRUE") == 0) item.art = true;
+      else if(art.compare("FALSE") == 0) item.art = false;
+      else return -4; //unrecognized art value
+      complete = complete | 0x1000; 
+    } else if(header.compare("RRTY") == 0){
+      if((complete & 0x2000) == 0x2000) return -1; //field already filled
+      item.rarity = stoi(line.substr(line.find_first_of(' ') + 1));
+      complete = complete | 0x2000;
+    }
+  }
+  if(complete != 0x3fff) return -2; //not all elements were initalized
+  items.push_back(item);
+  return 0;
+}
+
+void Dungeon::print_items(){
+  for(int i = 0; i < items.size(); i++){
+    print_item(i);
+  }
+}
+
+void Dungeon::print_item(int num){
+  //print name/description
+  cout << "Name: " << items[num].name << endl;
+  cout << "Description: " << endl;
+  cout << items[num].desc;
+  //print type
+  cout << items[num].type << endl;
+  //print colors
+  cout << "Color: ";
+  switch(items[num].color){
+  case BLUE_PAIR:
+    cout << "Blue";
+    break;
+  case GREEN_PAIR:
+    cout << "Green";
+    break;
+  case RED_PAIR:
+    cout << "Red";
+    break;
+  case MAGENTA_PAIR:
+    cout << "Magenta";
+    break;
+  case YELLOW_PAIR:
+    cout << "Yellow";
+    break;
+  case WHITE_PAIR:
+    cout << "White";
+    break;
+  case GRAY_PAIR:
+    //cout << "Gray";
+    cout << "Black";
+    break;
+  default:
+    cout << "Chartreuse?"; //this shouldn't happen
+  }
+  cout << endl;
+  //print hit
+  cout << "Hit Bonus: " << items[num].hit.toString() << endl;
+  //print damage
+  cout << "Damage Bonus: " << items[num].damage.toString() << endl;
+  //print dodge
+  cout << "Dodge Bonus: " << items[num].dodge.toString() << endl;
+  //print def
+  cout << "Defensive Bonus: " << items[num].def.toString() << endl;
+  //print weight
+  cout << "Weight: " << items[num].weight.toString() << endl;
+  //print speed
+  cout << "Speed Bonus: " << items[num].speed.toString() << endl;
+  //print attr
+  cout << "Special Attribute: " << std::to_string(items[num].attr) << endl;
+  //print val
+  cout << "Value: " << items[num].value.toString() << endl;
+  //print art (not the felony)
+  cout << "Artifact: ";
+  if(items[num].art) cout << "YES" << endl;
+  else cout << "NO" << endl;
+  cout << "Rarity: " << std::to_string(monsters[num].rarity) << endl;
+  cout << endl;
+}
