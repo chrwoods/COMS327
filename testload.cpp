b4d78b974cacbd651d36c6e7586e466ccccb2ae9
Christopher Woods <chrisscottwoods@gmail.com>
2019-04-03 17:17:55 -0500

    Added item loading to testload

--- testload.cpp
+++ testload.cpp
@@ -4,12 +4,23 @@
 
 int main(int argc, char* argv[]){
   Dungeon rlg;
-  string filepath = getenv("HOME");
-  filepath += "/.rlg327/monster_desc.txt";
+  string home = getenv("HOME");
+  //load monsters
+  string filepath = home + "/.rlg327/monster_desc.txt";
   int loaded = rlg.load_monsters(filepath);
   if(loaded < 0){
     printf("Problem encountered with loading monsters, exiting.\n");
     return loaded;
   }
+  //print monsters
   rlg.print_monsters();
+  //load items
+  filepath = home + "/.rlg327/object_desc.txt";
+  loaded = rlg.load_items(filepath);
+  if(loaded < 0){
+    printf("Problem encountered with loading items, exiting.\n");
+    return loaded;
+  }
+  //print items
+  rlg.print_items();
 }
